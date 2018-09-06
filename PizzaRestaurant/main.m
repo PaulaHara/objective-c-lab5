d03a62809ee62b1f04c16f0431edb3d2a8ad0ffd
paula <paulahara90@gmail.com>
2018-09-05 19:04:09 -0700

    Part 3

--- PizzaRestaurant/main.m
+++ PizzaRestaurant/main.m
@@ -29,7 +29,7 @@ int main(int argc, const char * argv[])
         NSInteger managerOption;
         
         while (![option isEqualToString:@"quit"]) {
-            NSLog(@"\n1 - Manager\n2 - Nice Manager\n3 - No Manager\n");
+            NSLog(@"\n1 - Manager\n2 - Nice Manager\n3 - No Manager\n4 - Delivered Pizzas\n");
             char strM[10];
             fgets (strM, 10, stdin);
             NSString *inputManager = [[NSString alloc] initWithUTF8String:strM];
@@ -56,49 +56,49 @@ int main(int argc, const char * argv[])
                 for(int i = 0; i < [pizzas count]; i++){
                     [strPizzas appendFormat:@"%@\n", [pizzas objectAtIndex:i]];
                 }
-                NSLog(@"Pizzas delivered:\n%@", strPizzas);
-            }
-            
-            NSLog(@"Please pick your pizza size and toppings:");
-            NSLog(@"> ");
-            char str[100];
-            fgets (str, 100, stdin);
-            
-            NSString *inputString = [[NSString alloc] initWithUTF8String:str];
-            inputString = [inputString stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-            
-            NSLog(@"Input was %@", inputString);
-            
-            // Take the first word of the command as the size, and the rest as the toppings
-            NSArray *commandWords = [inputString componentsSeparatedByString:@" "];
-            NSMutableArray *mutableCommands = [commandWords mutableCopy];
-            NSString *sizeCommand = @"";
-            
-            if([commandWords count] == 1){
-                option = [mutableCommands firstObject];
-            }else if([commandWords count] > 1){
-                sizeCommand = [mutableCommands firstObject];
-                [mutableCommands removeObjectAtIndex:0];
-                if([commandWords count] == 2){
+                NSLog(@"\nPizzas delivered:\n%@", strPizzas);
+            }else{
+                NSLog(@"Please pick your pizza size and toppings:");
+                NSLog(@"> ");
+                char str[100];
+                fgets (str, 100, stdin);
+                
+                NSString *inputString = [[NSString alloc] initWithUTF8String:str];
+                inputString = [inputString stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+                
+                NSLog(@"Input was %@", inputString);
+                
+                // Take the first word of the command as the size, and the rest as the toppings
+                NSArray *commandWords = [inputString componentsSeparatedByString:@" "];
+                NSMutableArray *mutableCommands = [commandWords mutableCopy];
+                NSString *sizeCommand = @"";
+                
+                if([commandWords count] == 1){
                     option = [mutableCommands firstObject];
-                }else{
-                    option = @"";
+                }else if([commandWords count] > 1){
+                    sizeCommand = [mutableCommands firstObject];
+                    [mutableCommands removeObjectAtIndex:0];
+                    if([commandWords count] == 2){
+                        option = [mutableCommands firstObject];
+                    }else{
+                        option = @"";
+                    }
                 }
-            }
-            
-            if([option isEqualToString:@"mozzarella"] && [sizeCommand isEqualToString:@""]){
-                pizza = [restaurantKitchen makeMozzarellaPizza];
-                [pizzaUtils printPizza:pizza];
-            }else if([option isEqualToString:@"tuna"]  && [sizeCommand isEqualToString:@""]){
-                pizza = [restaurantKitchen makeTunaPizza];
-                [pizzaUtils printPizza:pizza];
-            }else if([option isEqualToString:@"pepperoni"]){
-                pizza = [restaurantKitchen makePepperoniPizzaWithSize:getPizzaSizeByNSString(sizeCommand)];
-                [pizzaUtils printPizza:pizza];
-            }else if(![option isEqualToString:@"quit"]){
-                // And then send some message to the kitchen...
-                pizza = [restaurantKitchen makePizzaWithSize:getPizzaSizeByNSString(sizeCommand) toppings:mutableCommands];
-                [pizzaUtils printPizza:pizza];
+                
+                if([option isEqualToString:@"mozzarella"] && [sizeCommand isEqualToString:@""]){
+                    pizza = [restaurantKitchen makeMozzarellaPizza];
+                    [pizzaUtils printPizza:pizza];
+                }else if([option isEqualToString:@"tuna"]  && [sizeCommand isEqualToString:@""]){
+                    pizza = [restaurantKitchen makeTunaPizza];
+                    [pizzaUtils printPizza:pizza];
+                }else if([option isEqualToString:@"pepperoni"]){
+                    pizza = [restaurantKitchen makePepperoniPizzaWithSize:getPizzaSizeByNSString(sizeCommand)];
+                    [pizzaUtils printPizza:pizza];
+                }else if(![option isEqualToString:@"quit"]){
+                    // And then send some message to the kitchen...
+                    pizza = [restaurantKitchen makePizzaWithSize:getPizzaSizeByNSString(sizeCommand) toppings:mutableCommands];
+                    [pizzaUtils printPizza:pizza];
+                }
             }
         }
     }
